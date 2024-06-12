f0e87cc22ea8786967a4bc0cb7b07cf5a04318ac
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-12 17:04:16 +0200

    feat: add for loop to generate multiple random records

--- database/seeders/TrainsTableSeeders.php
+++ database/seeders/TrainsTableSeeders.php
@@ -14,16 +14,18 @@ class TrainsTableSeeders extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker\Factory::create('it_IT');
-        $newTrain = new Train();
-        $newTrain->company = $faker->randomElement(['Trenitalia', 'Italo', 'Trenord', 'OBB']);
-        $newTrain->departure_station = $faker->city();
-        $newTrain->arrival_station = $faker->city();
-        $newTrain->departure_time = $faker->dateTimeInInterval('-1 week', '+1 week');
-        $newTrain->arrival_time = $faker->dateTimeInInterval('-1 week', '+1 week');
-        $newTrain->train_code = $faker->numberBetween(1000, 9999);
-        $newTrain->number_of_carriages = $faker->numberBetween(1, 12);
-        $newTrain->cancelled = $faker->numberBetween(0, 1);
-        $newTrain->save();
+        for ($i = 0; $i < 100; $i++) {
+            $faker = Faker\Factory::create('it_IT');
+            $newTrain = new Train();
+            $newTrain->company = $faker->randomElement(['Trenitalia', 'Italo', 'Trenord', 'OBB']);
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->dateTimeInInterval('-1 week', '+1 week');
+            $newTrain->arrival_time = $faker->dateTimeInInterval('-1 week', '+1 week');
+            $newTrain->train_code = $faker->numberBetween(1000, 9999);
+            $newTrain->number_of_carriages = $faker->numberBetween(1, 12);
+            $newTrain->cancelled = $faker->numberBetween(0, 1);
+            $newTrain->save();
+        }
     }
 }
